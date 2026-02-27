666a09a23f40c5c2c9cb5ab5a489df5b64718176
ozcarprez <107212335+ozcarprez@users.noreply.github.com>
2026-02-26 17:48:14 -0800

    Add routing with BrowserRouter for Growi and Admin

--- src/main.jsx
+++ src/main.jsx
@@ -1,9 +1,16 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
+import { BrowserRouter, Routes, Route } from 'react-router-dom'
 import Growi from './Growi.jsx'
+import Admin from './Admin.jsx'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
-    <Growi />
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Growi />} />
+        <Route path="/admin" element={<Admin />} />
+      </Routes>
+    </BrowserRouter>
   </React.StrictMode>,
 )
